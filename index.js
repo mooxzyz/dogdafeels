74fc62f6bb1989f5f0f94734950919dd02c62cbe
Fusiion <91385547+mooxzyz@users.noreply.github.com>
2021-11-03 16:30:47 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let ans = 1
+
+function calc(num1, num2, op) {
+    if (op == '+') {
+        ans = num1 + num2
+    } else if (op == '*') {
+        ans = num1 * num2
+    }
+        console.log(ans)
+}
+
+
